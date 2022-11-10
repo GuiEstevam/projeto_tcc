14f97eeea1a81d6adfea61e044a4e3e8afb8c703
GuiEstevam <contato.estevamdelima@gmail.com>
2022-11-10 13:08:04 -0300

    merge main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,26 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<<<<<<< HEAD
-  <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>@yield('title')</title>
-      <!-- Fonte do Google -->
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-      <!-- CSS Bootstrap -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
-      <!-- CSS da aplicação -->
-      <link rel="stylesheet" href="/css/styles.css">
-      <link rel="stylesheet" href="/css/navbar.css">
-      <script src="/js/scripts.js"></script>
-      
-  </head>
-  <body>
-=======
 
 <head>
     <meta charset="utf-8">
@@ -44,7 +23,6 @@
 </head>
 
 <body>
->>>>>>> master
     <header>
         <nav class="navbar">
             <div class="navbar-container container">
